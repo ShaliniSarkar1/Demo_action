5268a9ea4ef0a37a554b68c52eff22e8dfd3a08f
Shalini Sarkar <81621815+ShaliniSarkar1@users.noreply.github.com>
2022-07-20 20:50:37 +0530

    Create main.tf

---
+++ aks-cluster/main.tf
@@ -0,0 +1,71 @@
+resource "random_pet" "prefix" {}
+
+terraform {
+
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "container-rg"
+    storage_account_name = "containerstorage28"
+    container_name = "tfstate"
+    key = "leap.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_kubernetes_cluster" "default" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = var.rg_name
+  dns_prefix          = "${random_pet.prefix.id}-k8s"
+
+
+  default_node_pool {
+    name            = "default"
+    node_count      = var.node
+    vm_size         = "Standard_D2_v2"
+    os_disk_size_gb = 30
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  role_based_access_control {
+    enabled = true
+  }
+
+  tags = {
+    createdby = "Shalini"
+  }
+}
+resource "kubernetes_namespace" "leap-task" {
+  metadata {
+    annotations = {
+      name = var.ns_name
+    }
+
+    labels = {
+      mylabel = "sample-app"
+    }
+
+    name = var.ns_name
+  }
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = var.rg_name
+  location            = var.location
+  sku                 = "Premium"
+  admin_enabled       = true
+}
