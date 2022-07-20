2528bb74b3a3e9ffdb0dc03310993e926b408ea4
Shalini Sarkar <81621815+ShaliniSarkar1@users.noreply.github.com>
2022-07-20 20:48:15 +0530

    Create variables.tf

---
+++ aks-cluster/variables.tf
@@ -0,0 +1,18 @@
+variable "rg_name" {
+  default = "container-rg"
+}
+variable "cluster_name" {
+ default = "cluster28"
+}
+variable "location" {
+ default = "northeurope"
+}
+variable "node" {
+ default = 2
+}
+variable "acr_name" {
+ default = "acr28"
+}
+variable "ns_name" {
+ default = "ns28"
+}
