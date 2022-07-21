fc00bdf85edb9062ee85f92d38980d7c6e3e6412
Shalini Sarkar <81621815+ShaliniSarkar1@users.noreply.github.com>
2022-07-21 11:52:21 +0530

    Create main.tf

---
+++ namespace/main.tf
@@ -0,0 +1,36 @@
+resource "random_pet" "prefix" {}
+
+terraform {
+
+  required_version = ">=0.12"
+
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+  backend "azurerm" {
+    resource_group_name = "container-rg"
+    storage_account_name = "containerstorage28"
+    container_name = "tfstate"
+    key = "leap.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+resource "kubernetes_namespace" "leap-task" {
+  metadata {
+    annotations = {
+      name = var.ns_name
+    }
+
+    labels = {
+      mylabel = "sample-app"
+    }
+
+    name = var.ns_name
+  }
+}
